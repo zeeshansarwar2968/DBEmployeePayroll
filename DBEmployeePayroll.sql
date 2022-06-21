5194f056c253d8ef16a30015eccdaf753a1c99dd
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-21 12:08:45 +0530

    [Zeeshan] : Added query for table creation

--- DBEmployeePayroll.sql
+++ DBEmployeePayroll.sql
@@ -2,3 +2,12 @@
 create database payroll_service;
 use payroll_service;
 
+
+--UC-2 creating Table
+create Table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
